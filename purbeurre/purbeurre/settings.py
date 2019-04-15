40a49842d7a5acd069ddc71ebae9825e0af29472
alxvnt <alixvoinot1@gmail.com>
2019-04-15 16:19:52 +0200

    add django-heroku

--- purbeurre/purbeurre/settings.py
+++ purbeurre/purbeurre/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 import dj_database_url
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -141,3 +142,5 @@ LOGIN_URL = '/connexion/'
 
 db_from_env = dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(db_from_env)
+
+django_heroku.settings(locals())
